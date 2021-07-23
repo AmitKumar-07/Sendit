ee46a0f68a8f5dcda71b8888be996be2fe71bf64
AmitKumar-07 <megurukumar@gmail.com>
2021-07-22 18:12:09 -0700

    organisation of code

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,13 @@
+//all queryselector
 const dropZone = document.querySelector(".drop-zone");
 const fileInput = document.querySelector("#fileInput");
 const browseBtn = document.querySelector("#browseBtn");
-
 
 const bgProgress = document.querySelector(".bg-progress");
 const progressPercent = document.querySelector("#progressPercent");
 const progressContainer = document.querySelector(".progress-container");
 const progressBar = document.querySelector(".progress-bar");
 const status = document.querySelector(".status");
-
 
 const sharingContainer = document.querySelector(".sharing-container");
 const copyURLBtn = document.querySelector("#copyURLBtn");
